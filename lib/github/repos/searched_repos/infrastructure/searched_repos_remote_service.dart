84ed46a7dccb5218adfe9e1c8e5ea4af6f203fdf
Ramin R <ramin.r9877@gmail.com>
2021-10-09 10:07:48 +0330

    extracted abstract class of repos remote service

--- lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
+++ lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
@@ -5,21 +5,23 @@ import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/repos_remote_service.dart';
 
-class StarredReposRemoteService extends ReposRemoteService {
-  StarredReposRemoteService(
+class SearchedReposRemoteService extends ReposRemoteService {
+  SearchedReposRemoteService(
     Dio dio,
     GithubHeadersCache headersCache,
   ) : super(dio, headersCache);
 
-  Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(int page) => super.getPage(
+  Future<RemoteResponse<List<GithubRepoDTO>>> getSearchedReposPage(String query, int page) =>
+      super.getPage(
         requestUri: Uri.https(
           'api.github.com',
-          '/user/starred',
+          '/search/repositories',
           {
+            'q': query,
             'page': page.toString(),
             'per_page': PaginationConfig.itemsPerPage.toString(),
           },
         ),
-        jsonDataSelector: (json) => json as List<dynamic>,
+        jsonDataSelector: (json) => json['items'] as List<dynamic>,
       );
 }
